f454d150fa1caff0c480ace618a8e9b8e4c0794f
michael dimmitt <michaelgdimmitt@gmail.com>
2019-05-26 18:11:26 -0400

    add fetch library

--- request.js
+++ request.js
@@ -1,40 +1,63 @@
 const axios = require('axios');
-var requestPromise = require('request-promise');
+const fetch = require('node-fetch');
+const requestPromise = require('request-promise');
 
-const printAxios = (msg) => {
-  console.log('\n',msg,' succeeded... library: axios')
-  console.log('path for accessing the response: "response.data"\n');
-  console.log(JSON.stringify(response.data), '\n');
+const print = (msg, lib, response) => {
+//   console.log({msg, lib, response})
+  console.log();
+  console.log(msg,' succeeded... library: ', lib)
+  console.log('lib:', lib, 'success:', JSON.stringify(response), '\n');
+  return response
 }
 
-const printRequestPromise = (msg, response) => {
-  // Process html...
-  console.log('\n',msg, ' succeeded... library: requestPromise')
-  console.log('path for accessing the response: "response"\n');
-  console.log(JSON.stringify(response), '\n')
-  return response.data
-}
-
-var options = {
-    method: 'POST', uri: 'https://httpbin.org/post',
-    body: { 'key': 'value' },
-    json: true // Automatically stringifies the body to JSON
-};
-
-// Make a request for a user with a given ID
+// Axios Library
 axios.get('http://www.example.com/')
-  .then(function (response) { printAxios('Get', response) })
-  .catch(function (error)   { console.log('error'); })
-
-const result = requestPromise('http://www.example.com')
-  .then(function (response) { printRequestPromise('Get', response) })
-  .catch(function (err)     { return 'err' });
+  .then(function (response) { print('Get', 'axios', response.data) })
+  .catch(function (error)   { console.log('Get lib: axios error:', {error}) })
 
 axios.post('https://httpbin.org/post', { firstName: 'Fred', lastName: 'Flintstone' })
-  .then(function (response) { printAxios('Get', response) })
-  .catch(function (error)   { console.log('POST failed...', error); });
+  .then(function (response) { print('Post', 'axios', response.data.data) })
+  .catch(function (error)   { console.log('POST lib: axios failed...', error); });
+
+// Request Library
+const options = {
+  method: 'POST', uri: 'https://httpbin.org/post',
+  body: { 'key': 'value' },
+  json: true // Automatically stringifies the body to JSON
+};
+
+const result = requestPromise('http://www.example.com')
+  .then(function (response) { print('Get', 'request', response) })
+  .catch(function (err)     { return 'err' });
 
 requestPromise(options)
-    .then(function (response) { printRequestPromise('Get', response) })
-    .catch(function (err) { console.log('POST failed...') });
+  .then(function (response) { print('Get', 'request', response.data) })
+  .catch(function (err) { console.log('POST lib: request failed...') });
+
+// // Fetch Library
+const postOptions = (data = {}) => {
+  const options = {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    mode: 'cors', // no-cors, cors, *same-origin
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
+        'Content-Type': 'application/json',
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: 'follow', // manual, *follow, error
+    referrer: 'no-referrer', // no-referrer, *client
+    body: JSON.stringify(data), // body data type must match "Content-Type" header
+  };
+  return options;
+}
+
+fetch('http://www.example.com/')
+  .then(function(response) { return response.text(); })
+  .then(function(response) { print('Get', 'fetch', response) })
+  .catch(function (error)   { console.log({error}) })
+
+fetch('https://httpbin.org/post', postOptions({answer: 42}) )
+  .then(response => response.json() )
+  .then(response => { print('Post', 'fetch', response.data) })
 
